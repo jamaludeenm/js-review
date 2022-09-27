52569d755506ac95e49f52fdf5da4dca87d21b8e
jamaludeen <justjamal17@gmail.com>
2022-09-27 13:14:01 +0530

    review

---
+++ index.js
@@ -0,0 +1,86 @@
+{
+
+// js datatype
+
+// 1,Number datatype
+let x1 = 34.00;     // Written with decimals
+let x2 = 34;        // Written without decimals
+
+// 2,string datatype
+let carName1 = "honda civic";
+
+// 3,Booleans datatype
+let x = 5;
+let y = 5;
+let z = 6;
+(x == y);       // Returns true
+(x == z);       // Returns false
+
+//4,Array datatype
+let favcolors = ["red","blue","yellow"]
+    favcolors[3]= "green"
+     console.log(favcolors)
+
+//5,function datatype
+//(function is defined with the function keyword, followed by a name, followed by parentheses ()
+   function add(number1,number2){
+	 console.log(number1+number2)
+   } 
+   add(54,453);
+   
+//6,objectdatatype
+{
+		let car={
+			brand:"honda",
+			model:"civic",
+			type:"diesel"
+		}
+		console.log(car.type);
+		console.log("<br>")	
+	}
+	
+//7,undefined datatype
+let car;
+console.log(car);        // undefined
+console.log(typeof car); // undefined
+	
+//null datatype
+var a = null;                    // its value is null and type is 'object'
+}
+
+
+{
+	//java math
+	
+	Math.PI;
+	
+	Math.round(4.6);
+	
+	Math.ceil(4.2);
+	Math.ceil(-4.2);
+	
+	Math.floor(4.2);
+    Math.floor(-4.2);
+	
+	Math.trunc(4.2);
+    Math.trunc(-4.2);
+	
+	Math.sign(-4);
+    Math.sign(0);
+    Math.sign(4);
+	
+	Math.pow(8, 2);
+	
+	Math.sqrt(64);
+	
+	Math.min(0, 150, 30, 20, -8, -200);
+	
+	Math.max(0, 150, 30, 20, -8, -200);
+	
+	Math.random();
+	
+    Math.log(10);
+	
+	Math.log2(8)
+
+}
